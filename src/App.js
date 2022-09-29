b32bae231f8952b3181f9dd796c1d8702b738a3d
Umarkhangit <umarkhan7299@gmail.com>
2022-09-29 11:28:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 // import './App.css';
 import Login from './lib/Login';
-import Login2 from './lib/Login2';
 import Home from './lib/Home';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
