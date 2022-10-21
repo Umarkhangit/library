e1d647a009b77220a886ce7a81fcdb634dc83e9d
roguecodes <ahmed_thabrez@yahoo.com>
2022-10-21 17:25:43 +0530

    changgges

--- src/lib/user/Account.jsx
+++ src/lib/user/Account.jsx
@@ -79,7 +79,7 @@ const Account = () => {
         return (a.books.isPenalty == true)
     })    
     
-
+    console.log(filteredEmp);  
           
     
 
@@ -96,10 +96,10 @@ const Account = () => {
               <MDBCol>
                 <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
                   <MDBBreadcrumbItem>
-                    <a href='#'>Home</a>
+                    {/* <a href='#'>Home</a> */}
                   </MDBBreadcrumbItem>
                   <MDBBreadcrumbItem>
-                    <a href="#">User</a>
+                    {/* <a href="#">User</a> */}
                   </MDBBreadcrumbItem>
                   <MDBBreadcrumbItem active>User Profile</MDBBreadcrumbItem>
                 </MDBBreadcrumb>
